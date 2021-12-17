67deba1341844d5e2a2c5002ab6c0586058cb8f0
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-12-17 22:58:59 +0900

    add leetCode #122 Best Time to Buy and Sell Stock

--- LeetCode/array/index.js
+++ LeetCode/array/index.js
@@ -31,7 +31,37 @@ function removeDuplicates(arr) {
   return arrayLength;
 }
 
+// #122
 removeDuplicates([0, 0, 1, 2, 4, 3, 2, 3, 1, 1, 0]);
 // [0,0,1,2,3,4,2,3,1,1,0]
 // returns the new length of array! [0,1,2,3,4]
 }
+
+
+function maxProfit(array) {
+  let profit = 0;
+  let stockPrice = 0; //
+  let hasStock = false;
+
+  for (let i = 0; i < array.length; i++) {
+    // buying condition
+    if (!hasStock && array[i] < array[i + 1]) {
+      stockPrice = array[i];
+      hasStock = true;
+    }
+    // selling condition
+    if (hasStock && array[i - 1] < array[i]) {
+      profit = profit + (array[i] - stockPrice);
+      stockPrice = 0;
+      hasStock = false;
+      console.log("profit", array[i], stockPrice, profit);
+      // DRY!
+      if (!hasStock && array[i] < array[i + 1]) {
+        stockPrice = array[i];
+        hasStock = true;
+      }
+    }
+  }
+  return profit;
+}
+console.log(maxProfit([2, 1, 2, 0, 2]));
