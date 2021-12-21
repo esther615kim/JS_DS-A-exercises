595367121a44f5f7ecc77eeda181e5952c24d1ab
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-12-22 06:17:06 +0900

    Add 136 Single number

--- LeetCode/array/index.js
+++ LeetCode/array/index.js
@@ -64,4 +64,21 @@ function maxProfit(array) {
   }
   return profit;
 }
-console.log(maxProfit([2, 1, 2, 0, 2]));
+// console.log(maxProfit([2, 1, 2, 0, 2]));
+
+// 136 Single Number
+
+function singleNumber(nums){
+  // Edge case
+  if(nums.length === 1) return nums[0];
+    
+    let result = [];
+    nums.sort((a, b) => a - b);
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== nums[i-1] && nums[i] !== nums[i+1]) {
+            result.push(nums[i]);
+        }
+    }
+    return result[0];
+}
