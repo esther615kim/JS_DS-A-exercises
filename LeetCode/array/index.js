92225290d345452ca2b4491bdd988559a56129da
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-12-23 03:27:42 +0900

    add #1 Two Sum

--- LeetCode/array/index.js
+++ LeetCode/array/index.js
@@ -97,3 +97,15 @@ function containDuplicate(nums){
     return false;
 
 }
+
+
+// leetCode 1 Two Sum
+var twoSum = function(nums, target) {
+      for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
